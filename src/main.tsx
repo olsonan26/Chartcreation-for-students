771549dd137fd25027e786d9c352c923450240f4
olsonan26 <youwillalertme@gmail.com>
2026-09-18 00:46:07 -0600

    Require login before loading student charts

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 
 import "../app/globals.css";
 import App from "../app/page";
+import { AuthGate } from "../features/auth/AuthGate";
+import { AuthProvider } from "../features/auth/AuthProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <AuthGate>
+        <App />
+      </AuthGate>
+    </AuthProvider>
   </StrictMode>,
 );
